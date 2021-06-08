978e38e4a033dcdd3284476bd1e6c9aecc8500ad
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-08 12:02:43 -0500

    added functional links to home page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function About() {
     return (
@@ -8,13 +9,19 @@ export default function About() {
             <p>For centuries, humans have looked to the heavens for guidance. Astrology can be used as a powerful and fun tool for understanding ourselves, others, and the world around us. This site provides guidance in three different areas: Horoscope, Moon Phases, and Tarot Card Reading.</p>
             <h1>Horoscope</h1>
             <p>A horoscope uses an astrological chart calculated from the planetary positions. The calculations are based on the date, place and time of birth. That is why horoscopes are extremely personal, like a fingerprint.</p>
-            <button>Get a Reading</button>
+            <Link to={"/horoscope"}>
+            <button>See Today's Horoscope</button>
+            </Link>
             <h1>Moon Phases</h1>
             <p>The phase the moon is in has a huge influence on your life, mindset, and mood. Working with the moon can strengthen our natural instincts.</p>
+            <Link to={"/moonphases"}>
             <button>See Current Moon Phase</button>
+            </Link>
             <h1>Tarot Cards</h1>
             <p>Tarot cards are used for divination and can give insight into your past, present, and future. A reading can offer a different perspective to a difficult situation or question you may have. We provide a three-card reading.</p>
+            <Link to={"/tarot"}>
             <button>Get a Reading</button>
+            </Link>
         </div>
     )
 }
