03a43d3c677cb6d7bb114f916361c36daa12791c
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-08 14:08:35 -0500

    added card component to horoscope

--- src/components/Horoscope.jsx
+++ src/components/Horoscope.jsx
@@ -47,7 +47,8 @@ export default function Horoscope() {
             <div className="zodiac-button">
                 <button onClick={() => horoscopeCall("cancer")}><img src={cancer} alt="zodiac"/><p className="dates">Jun 22<br></br>-<br></br>Jul 22</p></button>
             </div></div>
-            {horoscope[0].color ? <HoroscopeReading horoscope={horoscope}/> : <p></p>}
+            <div className="all-zodiac-buttons">
+            {horoscope[0].color ? <HoroscopeReading horoscope={horoscope}/> : <p></p>}</div>
             <div className="all-zodiac-buttons">
             <div className="zodiac-button">
                 <button onClick={() => horoscopeCall("leo")}><img src={leo} alt="zodiac"/><p className="dates">Jul 23<br></br>-<br></br>Aug 22</p></button>
--- src/components/HoroscopeReading.jsx
+++ src/components/HoroscopeReading.jsx
@@ -1,16 +1,58 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    maxWidth: 800,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 18,
+  },
+  pos: {
+    marginBottom: 12,
+    marginTop: 10,
+  },
+});
 
 export default function HoroscopeReading({horoscope}) {
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
     let signName = horoscope[1]
     let upperCasedSignName = signName.charAt(0).toUpperCase() + signName.slice(1)
     return (
         <div>
-            <h1>Today's {upperCasedSignName} Horoscope</h1>
-            <p>{horoscope[0].description}</p>
-            <p>You're compatible with {horoscope[0].compatibility} today.</p>
-            <p>Mood: {horoscope[0].mood}</p>
-            <p>Color: {horoscope[0].color}</p>
-            <p>Lucky Number: {horoscope[0].lucky_number}</p>
+             <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+        Today's {upperCasedSignName} Horoscope
+        </Typography>
+        <Typography variant="h5" component="h2">
+        {horoscope[0].description}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+        Mood {bull} {horoscope[0].mood}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+        Color {bull} {horoscope[0].color}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+        Lucky Number {bull} {horoscope[0].lucky_number}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+        Compatibility {bull} {horoscope[0].compatibility}
+        </Typography>
+        
+      </CardContent>
+    </Card>
         </div>
     )
 }
