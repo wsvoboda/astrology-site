2d79b0db1ad54ce57ffc033be229ee82b5fcdbb2
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-08 13:37:13 -0500

    created material ui menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,19 +6,42 @@ import Tarot from "./Tarot"
 import Error from "./ErrorPage"
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "../App.css"
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Fade from '@material-ui/core/Fade';
 
 export default function Navbar() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
     return (
         <div>
-            
-            <Router>
-                <nav className="navbar">
-                <Link to="/">About</Link>
-                <Link to="/horoscope">Horoscope</Link>
-                <Link to="/moonphases">Moon Phases</Link>
-                <Link to="/tarot">Tarot Card Reader</Link>
-                </nav>
-                <Switch>
+        <Router>
+      <Button aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick}>
+        Open Menu
+      </Button>
+      <Menu
+        id="fade-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+      >
+        <MenuItem onClick={handleClose}><Link to="/">About</Link></MenuItem>
+        <MenuItem onClick={handleClose}><Link to="/horoscope">Horoscope</Link></MenuItem>
+        <MenuItem onClick={handleClose}><Link to="/moonphases">Moon Phases</Link></MenuItem>
+        <MenuItem onClick={handleClose}><Link to="/tarot">Tarot Card Reader</Link></MenuItem>
+      </Menu>
+      <Switch>
                     <Route exact path="/" component={About} />
                     <Route exact path="/horoscope" component={Horoscope} />
                     <Route exact path="/moonphases" component={MoonPhases} />
@@ -26,7 +49,6 @@ export default function Navbar() {
                     <Route path="*" component={Error} />
                 </Switch>
             </Router>
-            
         </div>
     )
 }
