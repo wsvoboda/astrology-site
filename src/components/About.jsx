81dc72242d3647d3a9e838fc84b276ad95ea4c6a
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-08 10:54:01 -0500

    add info to landing page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,18 @@ export default function About() {
     return (
         <div>
             <h1>Welcome Page - About Us</h1>
+            <h1>As·trol·o·gy · /əˈsträləjē/</h1>
+            <p>noun: the study of the movements and relative positions of celestial bodies interpreted as having an influence on human affairs and the natural world</p>
+            <p>For centuries, humans have looked to the heavens for guidance. Astrology can be used as a powerful and fun tool for understanding ourselves, others, and the world around us. This site provides guidance in three different areas: Horoscope, Moon Phases, and Tarot Card Reading.</p>
+            <h1>Horoscope</h1>
+            <p>A horoscope uses an astrological chart calculated from the planetary positions. The calculations are based on the date, place and time of birth. That is why horoscopes are extremely personal, like a fingerprint.</p>
+            <button>Get a Reading</button>
+            <h1>Moon Phases</h1>
+            <p>The phase the moon is in has a huge influence on your life, mindset, and mood. Working with the moon can strengthen our natural instincts.</p>
+            <button>See Current Moon Phase</button>
+            <h1>Tarot Cards</h1>
+            <p>Tarot cards are used for divination and can give insight into your past, present, and future. A reading can offer a different perspective to a difficult situation or question you may have. We provide a three-card reading.</p>
+            <button>Get a Reading</button>
         </div>
     )
 }
