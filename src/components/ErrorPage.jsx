91b4fee011d45759f005776517196c726adcb76a
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-09 19:37:47 -0500

    style font and error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -4,6 +4,8 @@ export default function ErrorPage() {
     return (
         <div>
             <h1>Error 404</h1>
+            <h1>You're Lost in the Galaxy!</h1>
+            <h2>Use the links above to find your way home</h2>
         </div>
     )
 }
