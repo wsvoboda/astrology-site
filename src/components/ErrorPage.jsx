3d87ff12aa6a2714e3cf3cf51229ac28e9ab4181
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-10 14:34:19 -0500

    finish home page media queries

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -4,7 +4,7 @@ export default function ErrorPage() {
     return (
         <div>
             <h1>Error 404</h1>
-            <h1>You're Lost in the Galaxy!</h1>
+            <h2>You're Lost in the Galaxy!</h2>
             <h2>Use the links above to find your way home</h2>
         </div>
     )
--- src/components/HoroscopeReading.jsx
+++ src/components/HoroscopeReading.jsx
@@ -10,13 +10,14 @@ const useStyles = makeStyles({
     maxWidth: 800,
     fontFamily: "Cuprum",
     backgroundColor: "transparent",
-    textShadow: "2px 2px 10px black",
+    textShadow: "2px 2px 20px black",
     border: "none",
     boxShadow: "none",
   },
   inner: {
     fontFamily: "Cuprum",
     color: "white",
+    textShadow: "2px 2px 20px black",
   },
   bullet: {
     display: 'inline-block',
