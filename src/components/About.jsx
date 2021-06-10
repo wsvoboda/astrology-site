92c635d6a8a6dc7b825e3f88bf6475bf28bd4d0d
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-06-10 18:10:17 -0500

    update definition on homepage

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,13 +11,13 @@ export default function About() {
         <div>
             <div className="info-container">
             <div className="info">
-            <h1>As·trol·o·gy · /əˈsträləjē/</h1>
+            <h1 className="definition">As·trol·o·gy<br></br>·<br></br>/əˈsträləjē/</h1>
             <h2>noun: the study of the movements and relative positions of celestial bodies interpreted as having an influence on human affairs and the natural world</h2>
             <p>For centuries, humans have looked to the heavens for guidance. Astrology can be used as a powerful and fun tool for understanding ourselves, others, and the world around us. This site provides guidance in three different areas: Horoscope, Moon Phases, and Tarot Card Reading.</p>
             </div>
             <img id="constellations" src={horoscope} alt="constellations"/></div>
             <div className="info-box">
-            <div className="title-and-info">
+            <div className="info">
             <h1>Horoscope</h1>
             <p>A horoscope uses an astrological chart calculated from the planetary positions. The calculations are based on the date, place and time of birth. That is why horoscopes are extremely personal, like a fingerprint.</p>
             <Link to={"/horoscope"}>
@@ -28,7 +28,7 @@ export default function About() {
             <img id="zodiac-wheel" src={zodiac} alt="zodiac-wheel" />
             </div></div>
             <div className="middle-info-box">
-            <div className="title-and-info">
+            <div className="info">
             <h1>Moon Phases</h1>
             <p>The phase the moon is in has a huge influence on your life, mindset, and mood. Working with the moon can strengthen our natural instincts.</p><Link to={"/moonphases"}>
             <button className="buttons">See Current Moon Phase</button>
@@ -37,7 +37,7 @@ export default function About() {
             <img id="moon" src={moon} alt="moon" />
             </div></div>
             <div className="info-box">
-            <div className="title-and-info">
+            <div className="info">
             <h1>Tarot Cards</h1>
             
             <p>Tarot cards are used for divination and can give insight into your past, present, and future. A reading can offer a different perspective to a difficult situation or question you may have. We provide a three-card reading.</p><Link to={"/tarot"}>
